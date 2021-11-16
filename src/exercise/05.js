102c4f1855996f18e5d7ef9fb3b913cad7cff4f8
Chris Anderson <chris.anderson@zywave.com>
2021-11-16 12:46:52 -0500

    State Reducer Pattern

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,50 +1,54 @@
 // State Reducer
 // http://localhost:3000/isolated/exercise/05.js
 
-import * as React from 'react'
-import {Switch} from '../switch'
+import * as React from "react";
+import {Switch} from "../switch";
 
-const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
+const toggleReducerActionTypes = {
+  TOGGLE: "toggle",
+  RESET: "reset",
+};
+
+const callAll =
+  (...fns) =>
+  (...args) =>
+    fns.forEach(fn => fn?.(...args));
 
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
-      return {on: !state.on}
+    case toggleReducerActionTypes.TOGGLE: {
+      return {on: !state.on};
     }
-    case 'reset': {
-      return initialState
+    case toggleReducerActionTypes.RESET: {
+      return initialState;
     }
     default: {
-      throw new Error(`Unsupported type: ${type}`)
+      throw new Error(`Unsupported type: ${type}`);
     }
   }
 }
 
-// 🐨 add a new option called `reducer` that defaults to `toggleReducer`
-function useToggle({initialOn = false} = {}) {
-  const {current: initialState} = React.useRef({on: initialOn})
-  // 🐨 instead of passing `toggleReducer` here, pass the `reducer` that's
-  // provided as an option
-  // ... and that's it! Don't forget to check the 💯 extra credit!
-  const [state, dispatch] = React.useReducer(toggleReducer, initialState)
-  const {on} = state
+function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
+  const {current: initialState} = React.useRef({on: initialOn});
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const {on} = state;
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: toggleReducerActionTypes.TOGGLE});
+  const reset = () => dispatch({type: toggleReducerActionTypes.RESET, initialState});
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
-      'aria-pressed': on,
+      "aria-pressed": on,
       onClick: callAll(onClick, toggle),
       ...props,
-    }
+    };
   }
 
   function getResetterProps({onClick, ...props} = {}) {
     return {
       onClick: callAll(onClick, reset),
       ...props,
-    }
+    };
   }
 
   return {
@@ -53,33 +57,43 @@ function useToggle({initialOn = false} = {}) {
     toggle,
     getTogglerProps,
     getResetterProps,
-  }
+  };
 }
 
 function App() {
-  const [timesClicked, setTimesClicked] = React.useState(0)
-  const clickedTooMuch = timesClicked >= 4
+  const [timesClicked, setTimesClicked] = React.useState(0);
+  const clickedTooMuch = timesClicked >= 4;
 
   function toggleStateReducer(state, action) {
-    switch (action.type) {
-      case 'toggle': {
-        if (clickedTooMuch) {
-          return {on: state.on}
-        }
-        return {on: !state.on}
-      }
-      case 'reset': {
-        return {on: false}
-      }
-      default: {
-        throw new Error(`Unsupported type: ${action.type}`)
-      }
+    if (action.type === toggleReducerActionTypes.TOGGLE && clickedTooMuch) {
+      return {on: state.on};
     }
+
+    return toggleReducer(state, action);
   }
 
   const {on, getTogglerProps, getResetterProps} = useToggle({
     reducer: toggleStateReducer,
-  })
+  });
+
+  function ClickCount() {
+    return timesClicked > 0 ? (
+      <div data-testid="click-count">Click count: {timesClicked}</div>
+    ) : null;
+  }
+
+  function Notice() {
+    return (
+      <div data-testid="notice">
+        Whoa, you clicked too much!
+        <br />
+      </div>
+    );
+  }
+
+  function ClickCountOrNotice() {
+    return clickedTooMuch ? <Notice /> : <ClickCount />;
+  }
 
   return (
     <div>
@@ -90,22 +104,15 @@ function App() {
           onClick: () => setTimesClicked(count => count + 1),
         })}
       />
-      {clickedTooMuch ? (
-        <div data-testid="notice">
-          Whoa, you clicked too much!
-          <br />
-        </div>
-      ) : timesClicked > 0 ? (
-        <div data-testid="click-count">Click count: {timesClicked}</div>
-      ) : null}
+      {ClickCountOrNotice()}
       <button {...getResetterProps({onClick: () => setTimesClicked(0)})}>
         Reset
       </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 /*
 eslint
